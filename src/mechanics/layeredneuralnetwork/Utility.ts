dde1d1a51cf116e981c6d515d9b3ca04733fc0e3
Alex Johnson <ajohnson9551@gmail.com>
2022-05-05 17:11:22 -0500

    fully implement network

--- src/mechanics/layeredneuralnetwork/Utility.ts
+++ src/mechanics/layeredneuralnetwork/Utility.ts
@@ -2,10 +2,10 @@ import ActFunc from "./Layers/Enums/ActFunc";
 
 abstract class Utility {
 	static convertToVol(x: number[], inputSize: number[]) {
-		const vol: number[][][] = Array.from({length: inputSize[0]}, () => Array.from({length: inputSize[1]}, () => Array.from({length: inputSize[2]}, () => 0)));
-		for (let k: number = 0; k < inputSize[2]; k++) {
-			for (let j: number = 0; j < inputSize[1]; j++) {
-				for (let i: number = 0; i < inputSize[0]; i++) {
+		let vol = Array.from({length: inputSize[0]}, () => Array.from({length: inputSize[1]}, () => Array.from({length: inputSize[2]}, () => 0)));
+		for (let k = 0; k < inputSize[2]; k++) {
+			for (let j = 0; j < inputSize[1]; j++) {
+				for (let i = 0; i < inputSize[0]; i++) {
 					vol[i][j][k] = x[i + j * inputSize[0] + k * inputSize[1] * inputSize[0]];
 				}
 			}
@@ -14,7 +14,7 @@ abstract class Utility {
 	}
 
 	static convertToVolLin(x: number[]) {
-		const vol: number[][][] = Array.from({length: x.length}, () => Array.from({length: 1}, () => Array.from({length: 1}, () => 0)));
+		let vol = Array.from({length: x.length}, () => Array.from({length: 1}, () => Array.from({length: 1}, () => 0)));
 		for (let i in x) {
 			vol[i][0][0] = x[i];
 		}
@@ -22,24 +22,24 @@ abstract class Utility {
 	}
 
 	static convertToArr(vol: number[][][]) {
-		const arr: number[] = Array.from({length: vol.length * vol[0].length * vol[0][0].length}, () => 0);
-		for (let i: number = 0; i < arr.length; i++) {
+		let arr = Array.from({length: vol.length * vol[0].length * vol[0][0].length}, () => 0);
+		for (let i = 0; i < arr.length; i++) {
 			arr[i] = vol[i % vol.length][(i / vol.length) % vol[0].length][i / (vol.length * vol[0].length)];
 		}
 		return arr;
 	}
 
 	static dotProd(a: number[], x: number[]) {
-		let v: number = 0;
-		for (let i: number = 0; i < a.length; i++) {
+		let v = 0;
+		for (let i = 0; i < a.length; i++) {
 			v += a[i] * x[i];
 		}
 		return v;
 	}
 
 	static evaluate(A: number[][], b: number[], x: number[], actFuncE: ActFunc) {
-		let y: number[] = Array.from({length: A.length}, () => 0);
-		for (let i: number = 0; i < A.length; i++) {
+		let y = Array.from({length: A.length}, () => 0);
+		for (let i = 0; i < A.length; i++) {
 			y[i] += Utility.dotProd(A[i], x) + b[i];
 		}
 		Utility.actFuncify(y, actFuncE);
@@ -47,19 +47,23 @@ abstract class Utility {
 	}
 
 	static actFuncify(x: number[], actFuncE: ActFunc) {
-		let actFunc: (a: number) => number;
+		x = x.map((a) => {return this.actFunc(a, actFuncE)})
+	}
+
+	static actFunc(a: number, actFuncE: ActFunc) {
+		let f: (a: number) => number;
 		switch(actFuncE) {
 			case ActFunc.Relu:
-				actFunc = Utility.relu;
+				f = Utility.relu;
 				break;
 			case ActFunc.Sigmoid:
-				actFunc = Utility.sigmoid;
+				f = Utility.sigmoid;
 				break;
 			case ActFunc.Identity:
-				actFunc = Utility.identity;
+				f = Utility.identity;
 				break;
 		};
-		x = x.map((a) => {return actFunc(a)})
+		return f(a);
 	}
 
 	static sigmoid(a: number) {
@@ -72,6 +76,25 @@ abstract class Utility {
 
 	static identity(a: number) {
 		return a;
+	}
+
+	static getOrDefault(x: number[][][], i: number, j: number, k: number, def: number) {
+		if (i < 0 || i >= x.length || j < 0 || j >= x[0].length || k < 0 || k >= x[0][0].length) {
+			return def;
+		}
+		return x[i][j][k];
+	}
+
+	static maxIndex(x: number[]) {
+		let i = 0;
+		let max = x[0];
+		for (let j = 0; j < x.length; j++) {
+			if (x[j] > max) {
+				max = x[j];
+				i = j;
+			}
+		}
+		return i;
 	}
 }
 
