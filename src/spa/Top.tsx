ad67efc8866374e52371927eef0b74217184303d
Alex Johnson <ajohnson9551@gmail.com>
2022-05-30 20:53:20 -0500

    Make navbar sticky top

--- src/spa/Top.tsx
+++ src/spa/Top.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export const Top = () => {
 	return (
-		<Navbar bg="dark" variant="dark" expand="lg">
+		<Navbar bg="dark" variant="dark" expand="lg" sticky="top">
 			<Navbar.Toggle aria-controls="basic-navbar-nav"/>
 			<Navbar.Collapse id="basic-navbar-nav">
 				<Nav className="me-auto">
